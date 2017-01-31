c3f969a0455e15b2b7c64608680c35d588b8e337
91bsjun <91bsjun@gmail.com>
2017-01-31 16:37:28 +0530

    LDAU parameter edit (from pymatgen)

--- IKST/CCpyIKSTeditINCAR.py
+++ IKST/CCpyIKSTeditINCAR.py
@@ -3,10 +3,23 @@
 import os, sys
 
 # -- MAGMOM, LDA+U parameters
-mag = {"Ni":4.5, "Mn":4.5, "Co":4.5, "Li":0.0, "O":0.0}
-LDAUL = {"Ni":2, "Mn":2, "Co":2, "Li":2, "O":2}
-LDAUU = {"Ni":7.37, "Mn":5.84, "Co":6.14, "Li":0, "O":0}
-LDAUJ = {"Ni":1, "Mn":1, "Co":1, "Li":0, "O":0}
+# holee
+# mag = {"Ni":4.5, "Mn":4.5, "Co":4.5, "Li":0.0, "O":0.0}
+# LDAUL = {"Ni":2, "Mn":2, "Co":2, "Li":2, "O":2}
+# LDAUU = {"Ni":7.37, "Mn":5.84, "Co":6.14, "Li":0, "O":0}
+# LDAUJ = {"Ni":1, "Mn":1, "Co":1, "Li":0, "O":0}
+
+# pymatgen
+mag = {'Mn3+': 4, 'Ni4+': 0.6, 'Cr': 5, 'Mn4+': 3, 'Ta': 5, 'Ni3+': 1, 'Mo': 5,
+       'Ni': 2, 'V': 5, 'Mn2+': 5, 'Co': 5, 'Co4+': 1, 'W': 5, 'Fe3+': 5, 'Fe2+': 4,
+       'Mn': 5, 'Fe4+': 4, 'Fe': 5, 'Co3+': 0.6}
+LDAUL = {'Mo': 2, 'V': 2, 'Cu': 2, 'W': 2, 'Ag': 2, 'Cr': 2, 'Ta': 2,
+         'Nb': 2, 'Mn': 2, 'Re': 2, 'Co': 2, 'Ni': 2, 'Fe': 2}
+LDAUU = {'Mo': 4.38, 'V': 3.1, 'Cu': 4, 'W': 4.0, 'Ag': 1.5, 'Cr': 3.5, 'Ta': 2,
+         'Nb': 1.5, 'Mn': 3.9, 'Re': 2, 'Co': 3.4, 'Ni': 6, 'Fe': 4.0}
+LDAUJ = {'Mo': 0, 'V': 0, 'Cu': 0, 'W': 0, 'Ag': 0, 'Cr': 0, 'Ta': 0,
+         'Nb': 0, 'Mn': 0, 'Re': 0, 'Co': 0, 'Ni': 0, 'Fe': 0}
+
 
 try:
     dirname = sys.argv[1]
@@ -42,8 +55,7 @@ for supcell in supcells:
 
             mag_string = ""
             for i in range(len(n_of_atoms)):
-                for j in range(int(n_of_atoms[i])):
-                    mag_string += str(mag[elts[i]]) + " "
+                mag_strin += str(mag[elts[i]]) + "*" + str(n_of_atoms[i]) + " "
 
             LDAUL_string = ""
             for i in range(len(elts)):
@@ -60,9 +72,8 @@ for supcell in supcells:
             LDAU = """MAGMOM = %s
 
     # LDA+U parameters
-    LMAXMIX = 4
     LDAU = .TRUE.
-    LDAUTYPE = 1
+    LDAUTYPE = 2
     LDAUL = %s
     LDAUU = %s
     LDAUJ = %s"""%(mag_string, LDAUL_string, LDAUU_string, LDAUJ_string)
