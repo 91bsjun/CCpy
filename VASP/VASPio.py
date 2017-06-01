e1719a6d87d4f6c58b5921d58e9b44ae1ed557c3
91bsjun <91bsjun@gmail.com>
2017-06-01 15:43:27 +0900

    load json

--- VASP/VASPio.py
+++ VASP/VASPio.py
@@ -60,7 +60,7 @@ class VASPInput():
             incar_dict = load_json(home + "/.CCpy/vasp_incar.json", ordered=True)
         else:
             jstring = vasp_incar_json()         # Generate new INCAR
-            incar_dict = json.loads(jstring, ordered=True)
+            incar_dict = load_json(jstring, ordered=True)
             save_json(incar_dict, home + "/.CCpy/vasp_incar.json")
         # MAGMOM value preset check
         if "vasp_MAGMOM.json" in configs:
