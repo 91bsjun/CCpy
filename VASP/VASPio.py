2ba0a92e8c22e0d8267fea84ce94578a64005bf7
91bsjun <91bsjun@gmail.com>
2017-08-29 21:37:54 +0900

    change description

--- VASP/VASPio.py
+++ VASP/VASPio.py
@@ -749,7 +749,7 @@ class VASPOutput():
             target_name = filename
 
         cif.write_file(target_name)
-        print(target_name+" is generated.")
+        print(filename + " has been generated.")
 
         linux_command("mv "+target_name+" "+path)
 
