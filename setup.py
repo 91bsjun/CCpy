0c0cbe93417d99db028cafa3bdbdc25d07457051
91bsjun <91bsjun@gmail.com>
2017-11-23 18:18:42 +0900

    make setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='CCpy',
+    version='1.17',
+    packages=['', 'ATK', 'ATAT', 'CASM', 'IKST', 'VASP', 'Qchem', 'Queue', 'Tools', 'Package', 'Gaussian'],
+    url='https://github.com/91bsjun/CCpy',
+    license='',
+    author='Byeongsun Jun',
+    author_email='bjun915@gmail.com',
+    description=''
+)
