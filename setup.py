bfab9f45c24240b83ed8c0dfdcbb1530360c457f
91bsjun <91bsjun@gmail.com>
2018-06-14 10:58:59 +0900

    script files in setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,21 @@
 from distutils.core import setup
+import os
+
+bin_path = "./CCpy/bin/"
+script_files = [bin_path + f for f in os.listdir(bin_path)]
+script_files.sort()
+print(script_files)
 
 install_requires = [
+    'matplotlib'
     'pandas',
     'numpy',
     'pymatgen',
+    'tables',
 ]
 setup(
     name='CCpy',
-    version='1.20',
+    version='1.21',
     packages=['CCpy','CCpy/ATAT','CCpy/ATK','CCpy/CASM','CCpy/Gaussian',
               'CCpy/IKST','CCpy/Package','CCpy/Qchem','CCpy/Queue','CCpy/Tools','CCpy/VASP'],
     url='https://github.com/91bsjun/CCpy',
@@ -15,6 +23,6 @@ setup(
     author='Byeongsun Jun',
     author_email='bjun915@gmail.com',
     description='',
-    scripts=['CCpy/bin/*']
+    scripts=script_files
 
 )
