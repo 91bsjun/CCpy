717218033342ec3dd0ddec44132bf30bb3c314d3
root <root@node00.(none)>
2018-06-15 23:13:19 +0900

    raw input version

--- CCpy/VASP/CCpyVASPAnal.py
+++ CCpy/VASP/CCpyVASPAnal.py
@@ -4,6 +4,10 @@ import os, sys
 from CCpy.VASP.VASPio import VASPOutput
 from CCpy.Tools.CCpyTools import selectVASPInputs, selectVASPOutputs, selectInputs, linux_command
 
+ersion = sys.version
+if version[0] == '3':
+    raw_input = input
+
 try:
     chk = sys.argv[1]
 except:
