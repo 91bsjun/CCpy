35c42641e39097867130bdb3fc6f13ec87cabd9c
91bsjun <91bsjun@gmail.com>
2018-10-23 12:29:41 +0900

    Delete build files

--- build/lib/CCpy/Gaussian/myGausssum/tst.py
+++
@@ -1,5 +0,0 @@
-from matplotlib import pyplot as plt
-plt.figure(figsize=(6,5))
-x = [1,2,3]
-plt.plot(x, x)
-plt.show()
--- build/lib/CCpy/Tools/__init__.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) Byeongsun Jun, 2016.
-# Distributed under the terms of the "CCpy".
-# Whole scripts are not to be distributed without permission from author.
-
-"""
-Some Tools
-"""
-
