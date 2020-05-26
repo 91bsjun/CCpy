0a35cf165463e4cfb03f46c96609123a1323bc8a
root <91bsjun@gmail.com>
2020-05-26 14:48:12 +0900

    update g09 input

--- CCpy/Gaussian/CCpyGaussianInputGen.py
+++ CCpy/Gaussian/CCpyGaussianInputGen.py
@@ -27,9 +27,26 @@ version = sys.version
 if version[0] == '3':
     raw_input = input
 
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
 ask = True
+own_preset = False
 if "a" in sys.argv:
     ask = False
+
+for arv in sys.argv:
+    if "a" in arv:
+        ask = False
+    if ".json" in arv:
+        own_preset = arv
 
 if sys.argv[1] == "1":
     input_marker = [".xsd", ".xyz", "car"]
@@ -44,14 +61,20 @@ elif sys.argv[1] == "4":
     input_marker = [".com"]
     inputs = selectInputs(input_marker, "./")
 
-
 home = os.getenv("HOME")
 if ".CCpy" not in os.listdir(home):
     os.mkdir(home+"/.CCpy")
 configs = os.listdir(home+"/.CCpy")
-if "g09_input.json" in configs:
+
+preset_file = None
+if own_preset:
+    jstring = open(own_preset, "r").read()
+    options = json.loads(jstring)
+    preset_file = own_preset
+elif "g09_input.json" in configs:
     jstring = open(home + "/.CCpy/g09_input.json", "r").read()
     options = json.loads(jstring)
+    preset_file = home + "/.CCpy/g09_input.json"
 else:
     # -- option preset
     options = {"nproc":24, "mem":64, "functional":"B3LYP", "basis":"6-31G*",
@@ -60,13 +83,18 @@ else:
                "options_under_coordinates":""}
 
 # ------ basic option edit ------ #
-print("\n\n------------- Preset of basic options -------------")
+if preset_file:
+    print(bcolors.OKGREEN + "Preset read from: %s" % preset_file + bcolors.ENDC)
+else:
+    print("No preset file found.")
+
+print(bcolors.OKBLUE + "# --------------- Current basic options --------------- #" + bcolors.ENDC)
 for key in options.keys():
     if "options" not in str(key):
         print(str(key) + "="+str(options[key]))
 
-get_sets = raw_input("\n* Anything want to modify ? if not, enter \"n\". \nif you have (ex: basis=gen,functional=Cam-B3LYP) \n: ")
-if get_sets != "n":
+get_sets = raw_input(bcolors.WARNING + "\n* Anything want to modify ? if not, type Enter. \nIf you have (ex: basis=gen,functional=Cam-B3LYP)" + bcolors.ENDC + "\n: ")
+if len(get_sets) > 0:
     vals = get_sets.replace(" ","")
     vals = vals.split(",")
     for val in vals:
@@ -80,20 +108,20 @@ if get_sets != "n":
         options[key] = value
 
 # ------ calc option edit ------ #
-print("\n\n-------------- Calculation options ---------------")
+print(bcolors.OKBLUE + "# --------------- Current calculation options --------------- #" + bcolors.ENDC)
 print("Current option : " + options["options"])
-print("Option blocks : gfinput gfprint SCF(maxcycle=512,conver=6) opt=gediis freq=noraman pop=full iop(3/33=1,3/36=-1) pseudo=read EmpiricalDispersion=GD3 nosym sp")
-get_options = raw_input("Enter options to modify (ex: gfinput gfprint nosym opt=gediis) if not, enter \"p\" \n :")
-if get_options != "p":
+print("examples : gfinput gfprint SCF(maxcycle=512,conver=6) opt=gediis freq=noraman pop=full iop(3/33=1,3/36=-1) pseudo=read EmpiricalDispersion=GD3 nosym sp")
+get_options = raw_input(bcolors.WARNING + "Enter options to modify (ex: gfinput gfprint nosym opt=gediis) if not, type enter" + bcolors.ENDC + "\n:")
+if len(get_options) > 0:
     options["options"] = get_options
 
 # ------ bottom option edit ------ #
-print("\n\n--------- Options under coordinates area ---------")
+print(bcolors.OKBLUE + "# --------------- Current options under coordinates area --------------- #" + bcolors.ENDC)
 if len(options["options_under_coordinates"]) < 2:
     print("Current option : Empty")
 else:
     print("Current option : \n" + options["options_under_coordinates"])
-print("If you want to use current option, enter \"p\". \n Or fill line.")
+print(bcolors.WARNING + "If you want to use current option, enter \"p\". \n Or fill line." + bcolors.ENDC)
 print("Enter \"n\" when you finished.")
 print("""(example)
 line: I 0
@@ -117,21 +145,23 @@ while line_option != "n" and line_option != "p":
     if line_option != "n" and line_option != "p":
         get_options += line_option+"\n"
 options["options_under_coordinates"] = get_options
+
 chk = raw_input("Do you want to update preset option? (y/n)")
 if chk == "y":
     jstring = json.dumps(options, indent=4)
     f = open(home + "/.CCpy/g09_input.json", "w")
     f.write(jstring)
     f.close()
+
 myGI = GI(nproc=options['nproc'], mem=options['mem'],
           functional=options['functional'], basis=options['basis'],
           chg=options['chg'], multi=options['multi'],
           options=options['options'], options2=options['options_under_coordinates'])
 
-try:
+
+if 'check' not in os.listdir("./"):
     os.mkdir("check")
-except:
-    pass
+
 for each_input in inputs:
     if sys.argv[1] == "1":
         myGI.newCalc(each_input)
